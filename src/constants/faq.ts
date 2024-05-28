a8adf59ba545f93340f6efabb4b5d54ed662afb2
Bekzod <bekzodnorkulov9936@gmail.com>
2024-05-28 14:38:43 +0500

    fix: faq

--- src/constants/faq.ts
+++ src/constants/faq.ts
@@ -1,22 +1,52 @@
 export const FAQ = [
 	{
-		title: 'Is there a free trial available?',
-		text: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.',
+		title: 'what_type_of_businesses_is_your_platform_suitable_for',
+		text: {
+			title: 'our_platform_is_suitable_for_businesses_in_the_food_service_and_retail_sectors',
+			list: [
+				'for_food_service_robosell_is_ideal_for_restaurants_cafes',
+				'for_retail_stores_if_you_own_a_retail_store_and_want_to_sell_your_products_online',
+			],
+		},
 	},
 	{
-		title: 'Can I change my plan later?',
-		text: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.',
+		title: 'is_there_a_free_version',
+		text: {
+			list: [
+				'yes_you_can_use_the_full_functionality_for_creating_a_telegram_bot_and_your_own_website_for_the_first_7_days',
+			],
+		},
 	},
 	{
-		title: 'Can other info be added to an invoice?',
-		text: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.',
+		title: 'how_do_i_create_a_website',
+		text: {
+			title: 'when_registering_on_the_robosell_platform',
+			list: [
+				'registration_register_on_the_robosell_platform',
+				'project_type_selection_choose_the_option_to_create_a_website',
+				'domain_setup_add_your_own_domain_or_use_a_subdomain_provided_by_the_platform',
+				'design_and_content_customize_your_website_s_design_add_necessary_information_products_and_services',
+				'launch_after_setting_up_and_verifying_all_parameters_launch_your_website_and_start_receiving_orders',
+			],
+		},
 	},
 	{
-		title: 'How does billing work?',
-		text: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.',
+		title: 'how_do_i_create_a_telegram_bot',
+		text: {
+			title: 'when_registering_on_the_robosell_platform_you_will_be_offered',
+			list: [
+				'registration_register_on_the_robosell_platform',
+				'project_Type_selection_choose_the_option_to_create_a_telegram_bot',
+				'enter_token_enter_your_bot_token_which_you_can_get_by_creating_a_bot_through_botfather_on_telegram',
+				'setup_configure_the_functions_and_commands_of_your_bot_using_the_platform_s_user_friendly_interface',
+				'launch_after_completing_the_setup_activate_your_bot_and_start_using_it_for_your_business',
+			],
+		},
 	},
 	{
-		title: 'How do I change my account email?',
-		text: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.',
+		title: 'can_i_integrate_online_payments',
+		text: {
+			list: ['yes_for_your_convenience'],
+		},
 	},
 ]
--- src/routes/home/components/faq/faq.tsx
+++ src/routes/home/components/faq/faq.tsx
@@ -2,13 +2,15 @@ import { FAQ } from '@/constants/faq'
 import Stack from '@mui/material/Stack'
 import Divider from '@mui/material/Divider'
 import { Wrapper, Container } from './style'
-import { useTranslation } from 'next-i18next'
 import Accordion from '@mui/material/Accordion'
 import { IconMinus } from '@/assets/icons/minus'
 import Typography from '@mui/material/Typography'
+import { Trans, useTranslation } from 'next-i18next'
 import { useState, type SyntheticEvent } from 'react'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import AccordionSummary from '@mui/material/AccordionSummary'
+
+const BOT_FATHER = 'https://t.me/BotFather'
 
 export const Faq = () => {
 	const { t } = useTranslation('common')
@@ -39,7 +41,7 @@ export const Faq = () => {
 				>
 					{t('everything_you_need_to_know_about_the_product_and_billing')}
 				</Typography>
-				<Stack gap={{ xs: '8px', md: '15px' }}>
+				<Stack width='100%' gap={{ xs: '8px', md: '15px' }}>
 					{FAQ.map(({ text, title }, i: number) => {
 						const isExpanded = expanded === i
 						return (
@@ -50,7 +52,7 @@ export const Faq = () => {
 									expandIcon={isExpanded ? <IconMinus /> : undefined}
 								>
 									<Typography variant='title100' component='h4'>
-										{title}
+										{t(title)}
 									</Typography>
 								</AccordionSummary>
 								<AccordionDetails>
@@ -60,9 +62,44 @@ export const Faq = () => {
 											borderColor: theme.palette.colors.GRAY160,
 										})}
 									/>
-									<Typography variant='text90' component='p' pr={{ xs: '0', md: '50px' }}>
-										{text}
-									</Typography>
+									{text.title && (
+										<Typography mb='10px' variant='title60' component='h3'>
+											{t(text.title)}
+										</Typography>
+									)}
+									<Stack gap='5px' component='ul'>
+										{text.list.map((title, i: number) => {
+											return (
+												<Typography
+													key={i}
+													component='li'
+													variant='text90'
+													pr={{ xs: '0', md: '50px' }}
+												>
+													-{' '}
+													{title ===
+													'enter_token_enter_your_bot_token_which_you_can_get_by_creating_a_bot_through_botfather_on_telegram' ? (
+														<Trans
+															i18nKey={title}
+															components={{
+																linkTag: (
+																	<Typography
+																		component='a'
+																		target='_blank'
+																		variant='inherit'
+																		href={BOT_FATHER}
+																		color={theme => theme.palette.colors.PURPLE}
+																	/>
+																),
+															}}
+														/>
+													) : (
+														t(title)
+													)}
+												</Typography>
+											)
+										})}
+									</Stack>
 								</AccordionDetails>
 							</Accordion>
 						)
