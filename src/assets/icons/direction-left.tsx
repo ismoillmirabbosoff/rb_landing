743b94106e6e728d8e484df93078f90725f0ced9
Bekzod <bekzodnorkulov9936@gmail.com>
2024-07-02 15:01:54 +0500

    fix: icon

--- src/assets/icons/direction-left.tsx
+++ src/assets/icons/direction-left.tsx
@@ -1,13 +1,13 @@
 export const IconDirectionLeft = () => {
 	return (
 		<svg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
-			<circle cx='24' cy='24' r='24' fill='gray' fill-opacity='0.25' />
+			<circle cx='24' cy='24' r='24' fill='gray' fillOpacity='0.25' />
 			<path
 				d='M23 19L27 24L23 29'
 				stroke='white'
-				stroke-width='1.5'
-				stroke-linecap='round'
-				stroke-linejoin='round'
+				strokeWidth='1.5'
+				strokeLinecap='round'
+				strokeLinejoin='round'
 			/>
 		</svg>
 	)
