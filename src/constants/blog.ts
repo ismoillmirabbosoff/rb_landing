8c500c2d79772b7ff6dfb3d0d99a3fa8a6188ba2
Bekzod <bekzodnorkulov9936@gmail.com>
2024-07-29 12:14:45 +0500

    fix: blog

--- src/constants/blog.ts
+++ src/constants/blog.ts
@@ -9,6 +9,11 @@ import ImageBlog14 from '@/assets/images/blog-1.4.png'
 import ImageBlog21 from '@/assets/images/blog-2.1.png'
 import ImageBlog22 from '@/assets/images/blog-2.2.png'
 import ImageBlog23 from '@/assets/images/blog-2.3.png'
+import ImageBlog31 from '@/assets/images/blog-3.1.png'
+import ImageBlog32 from '@/assets/images/blog-3.2.png'
+import ImageBlog33 from '@/assets/images/blog-3.3.png'
+import ImageBlog34 from '@/assets/images/blog-3.4.png'
+import ImageBlog35 from '@/assets/images/blog-3.5.png'
 
 export interface BlogProps {
 	id: string
@@ -82,11 +87,47 @@ export const BLOGS: BlogProps[] = [
 		],
 	},
 	{
-		id: '4',
+		id: '3',
 		isMain: true,
 		date: dayjs(),
 		photo: ImageBlog3.src,
-		title: 'Most popular design systems to learn from in 2022',
-		content: [],
+		title: 'How to Start a Business Online Using One Platform',
+		content: [
+			{
+				image: ImageBlog31.src,
+				texts: [
+					'Robosell platformasi biznesga onlayn kirish jarayonini soddalashtiradi, xavflarni kamaytiradi va ishga tushirishni sezilarli darajada tezlashtiradi.',
+				],
+			},
+			{
+				image: ImageBlog32.src,
+				texts: [
+					'O‘zbekistonda elektron tijorat jadal sur’atlar bilan o‘sib bormoqda, biroq ko‘plab tadbirkorlar bu borada muvaffaqiyatga erisha olmayaptilar. Bu bilim va imkoniyatlar  o’rtasida bo’shliqni keltirib chiqaradi. Robosell.uz platformasi biznesga onlayn kirish jarayonini soddalashtiradi, xavflarni kamaytiradi va tashkiliy jarayonlarni  sezilarli darajada tezlashtiradi.',
+					'Endi biznesingizni onlayn boshlash uchun dasturchilar yoki kompaniyalarni qidirishingiz, ulardan 15-20 kunlab javob kutishingiz, shuningdek, Telegram bot yoki veb-saytga o‘zgartirish kiritmoqchi bo‘lganingizda har safar dasturchilarga murojaat qilishingiz shart emas.',
+				],
+			},
+			{
+				image: ImageBlog33.src,
+				texts: [
+					'Keling, solishtirib ko‘ramiz: Robosell platformasida Telegram bot hamda veb-sayt yaratishning umumiy oylik narxi 400 000 so‘m (tarifga qarab)ga teng bo‘ladi. Yiliga bu summa 3 840 000 so‘mni tashkil qiladi.',
+					'Agar bu ishni o‘zingiz qilmoqchi bo‘lsangiz: Telegram bot (Web App versiyasi) – 8 mln so’m. Veb-sayt – 25 mln so’mdan -38 mln so’mgacha. Admin paneli – 19 mln so’m. Texnik qo‘llab-quvvatlash – 8 mln so’m/1 yil. Virtual server – 150 000 so‘m/1 oy, yiliga 1 800 000 so‘m. Umumiy hisobda, yiliga taxminan 68 mln so’m ketadi.',
+				],
+			},
+			{
+				image: ImageBlog34.src,
+				texts: [
+					'Robosell platformasi bilan tadbirkor nafaqat pulini, balki asablarini ham saqlaydi, chunki  birinchi urinishdayoq xohlagan narsaga erishishga hech kim kafolat bermaydi. Buning ustiga mijoz xohishini bitta so‘zdan tushunadigan va bajara oladigan jamoani topish – eng qiyin masala.',
+					'Bundan xulosa qilamizki, tadbirkor o’zi ishlab chiqaruvchilarga alohida mablag‘ sarflab yaratgan platformaning’ o‘rniga shu pul evaziga Robocell bilan taxminan 15-17 yil davomida muvaffaqiyatli biznes yuritishi mumkin.',
+					'Hozirda har kuni 50 dan ortiq foydalanuvchiRobosell.uz’da ro’yxatdan o’tib, platformani asta-sekin o’z biznesiga joriy qilmoqda.',
+				],
+			},
+			{
+				image: ImageBlog35.src,
+				texts: [
+					'Ayni paytda platforma orqali siz o’zingizning Telegram botingiz va veb-saytingizni yaratishingiz mumkin. Eng muhimi, biznesni boshqarish, uni ko’plab xizmatlar bilan integratsiya qilish uchun boshqaruv paneliga ega bo’lasiz.',
+					'Foydalanuvchilarimizga yanada qulaylik yaratish uchun biz Call-markazni ishga tushirganmiz. Bu yerda ular nafaqat barcha savollarga javob olishlari, balki yangi akkaunt yaratish va biznesni boshlashda qo‘llab-quvvatlashga ega bo‘ladilar. Kompaniyamiz Genius Quality Agency, Robosell menejerlarini o’qitadi va ularni mijozlarni jalb qilish bo’yicha professional maslahatchilarga aylantiradi.',
+				],
+			},
+		],
 	},
 ]
