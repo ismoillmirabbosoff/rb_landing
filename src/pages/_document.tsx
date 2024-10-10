2d70dcbeceb6ba5318e7d8c0bb893942b27a3ce2
Bekzod <bekzodnorkulov9936@gmail.com>
2024-10-10 15:48:28 +0500

    feat: google tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,8 +21,29 @@ interface MyDocumentProps extends DocumentProps {
 export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
 	return (
 		<Html lang='en' className={gilroy.className}>
-			<Head>{emotionStyleTags}</Head>
+			<Head>
+				{emotionStyleTags}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KBPK8JZS');
+            `,
+					}}
+				/>
+			</Head>
 			<body>
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBPK8JZS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+					}}
+				/>
 				<Main />
 				<NextScript />
 			</body>
